5242d87e40d37262ebc2f7d69a7cc1ce91c53440
dsowles <d_sowles@mail.fhsu.edu>
2026-02-09 19:10:44 -0600

    Summary: Completed the first two tasks of the assignment.
    Details:
    Task1 - Created a function component to showcase the use of JSX via
            function definition.
    Task2 - Created a class definition to showcase the use of JSX via
            a class definition.

--- src/App.js
+++ src/App.js
@@ -6,14 +6,92 @@ Description:  Root component of the application, responsible for high-level
               layout and provider initialization.
 ******************************************************************************/
 
+
+/******************************************************************************
+Imports
+******************************************************************************/
+
+import React, {Component} from 'react';
+
+/*****************************************************************************/
+
+
+/******************************************************************************
+Class Component Definitions
+******************************************************************************/
+
+// Class components must inherit from the Component Parent class
+class UserInfo extends Component {
+  // The constructor of the class component will expect an object
+  // which encapsulates the JSX properties which were passed to
+  // the component (assuming some were passed).
+  constructor(props) {
+    // Make sure to call the parent's constructor first.
+    super(props);
+    // In this case, the class component's information will simply
+    // be defined as a class field.
+    this.state = {
+      name: "Gordan Freeman",
+      profession: "Theoretical Physicist"
+    };
+    this.luckyNumber =  Math.floor(Math.random() * 100) + 1;
+  }
+
+  // Class components must define a render function which returns
+  // the required JSX for the component.
+  render() {
+    return (
+      <div style={{fontSize:24}}>
+        {/* Task: Use JSX to return name and profession */}
+        <h2>User Profile</h2>
+        <p>Name: {this.state.name}</p>
+        <p>Profession: {this.state.profession}</p>
+        <p>Your lucky number is: {this.luckyNumber}</p>
+      </div>
+    );
+  }
+}
+
+/*****************************************************************************/
+
+
+/******************************************************************************
+Function Component Definitions
+******************************************************************************/
+
+function Greeting() {
+  return (
+
+    <>
+      <h1>Hello, Welcome to React!</h1>
+      <p style={{
+        color: "cadetblue",
+        fontSize: 24,
+        backgroundColor: "peachpuff",
+        width: 180
+      }}>
+        This is going to be fun!
+      </p>
+
+      <p style={{fontSize:24}}>Today's date and time is: {(new Date()).toLocaleString()}</p>
+    </>
+
+  );
+}
+
 // Creates a functional component that setups up the overall layout of the
 // React app. This is also where the React app's "root" element will be.
 function App() {
   return (
     <main>
-      <h1>Hello, World!</h1>
+      <Greeting />
+      <br />
+
+      <UserInfo />
     </main>
   );
 }
+
+/*****************************************************************************/
 
 export default App;
